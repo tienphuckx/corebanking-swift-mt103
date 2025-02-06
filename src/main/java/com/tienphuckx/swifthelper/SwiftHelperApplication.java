65d2ac68178570d6d22f788239c00f737930cb22
tienphuckx <tienphuckx@gmail.com>
2025-02-06 09:52:44 +0700

    init mt103 helper project

---
+++ src/main/java/com/tienphuckx/swifthelper/SwiftHelperApplication.java
@@ -0,0 +1,13 @@
+package com.tienphuckx.swifthelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tienphuckx/swifthelper/SwiftHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienphuckx.swifthelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
